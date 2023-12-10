9f06f6daf4df19187fc889cbc91b7fb7144a76cb
Aleksandr Antashkevich <antoha_ale6@mail.ru>
2023-12-10 13:16:16 +0300

    HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,21 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort': { // by name
+        case 'sort': {
+            if (action.payload === 'up') {
+                state.sort((a, b) => a.name > b.name ? 1 : -1);
+            } 
 
-            return state // need to fix
+            if (action.payload === 'down') {
+                state.sort((a, b) => a.name < b.name ? 1 : -1);
+            }
+
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(user => user.age >= action.payload)
         }
         default:
             return state
